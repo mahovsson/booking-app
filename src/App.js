bfd10461f29996211f51e82dd6cb707302360a6b
mahovsson <matus137@gmail.com>
2021-05-18 19:13:37 +0200

    Main scope with routes and navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
 import './App.css';
+import Home from './booking/Home';
+import Register from './auth/Register';
+import Login from './auth/Login';
+import Navigation from './components/Navigation.js/Navigation';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
-	return <div className='App'>Hello World</div>;
+	return (
+		<Router>
+			<Navigation />
+			<Switch>
+				<Route exact path='/'>
+					<Home />
+				</Route>
+				<Route path='/login'>
+					<Login />
+				</Route>
+				<Route path='/register'>
+					<Register />
+				</Route>
+			</Switch>
+		</Router>
+	);
 }
 
 export default App;
