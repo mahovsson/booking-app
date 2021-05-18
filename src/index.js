ac829046a80dac85fe73e181fc8de24aaf4bb4d6
mahovsson <matus137@gmail.com>
2021-05-18 19:47:58 +0200

    redux setup

--- src/index.js
+++ src/index.js
@@ -2,10 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import rootReducer from './reducers/index';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
+const store = createStore(rootReducer, composeWithDevTools());
 
 ReactDOM.render(
 	<React.StrictMode>
-		<App />
+		<Provider store={store}>
+			<App />
+		</Provider>
 	</React.StrictMode>,
 	document.getElementById('root')
 );
---
+++ src/reducers/auth.js
@@ -0,0 +1,12 @@
+const authReducer = (state = {}, action) => {
+	switch (action.type) {
+		case 'LOGGED_IN':
+			return { ...state, ...action.payload };
+		case 'LOGGED_OUT':
+			return action.payload;
+		default:
+			return state;
+	}
+};
+
+export default authReducer;
