525082fdaf936394d20f30ecb5690386a243666b
mahovsson <matus137@gmail.com>
2021-05-25 19:58:42 +0200

    register toastify added

--- src/App.js
+++ src/App.js
@@ -4,19 +4,32 @@ import Register from './auth/Register';
 import Login from './auth/Login';
 import Navigation from './components/Navigation.js/Navigation';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
 	return (
 		<Router>
 			<Navigation />
+			<ToastContainer
+				position='bottom-right'
+				autoClose={5000}
+				hideProgressBar={false}
+				newestOnTop={false}
+				closeOnClick
+				rtl={false}
+				pauseOnFocusLoss
+				draggable
+				pauseOnHover
+			/>
 			<Switch>
 				<Route exact path='/'>
 					<Home />
 				</Route>
-				<Route path='/login'>
+				<Route exact path='/login'>
 					<Login />
 				</Route>
-				<Route path='/register'>
+				<Route exact path='/register'>
 					<Register />
 				</Route>
 			</Switch>
--- src/auth/Register.js
+++ src/auth/Register.js
@@ -1,23 +1,40 @@
-import React, { Fragment, useState } from 'react';
+import React, { useState } from 'react';
 import RegisterForm from '../components/RegisterForm';
 import axios from 'axios';
+import { toast } from 'react-toastify';
+import { useHistory } from 'react-router-dom';
 
 const Register = () => {
 	const [name, setName] = useState('');
 	const [email, setEmail] = useState('');
 	const [password, setPassword] = useState('');
+	const history = useHistory();
 
 	const handleSubmit = async (e) => {
 		e.preventDefault();
 		try {
-			const res = await axios.post(`http://localhost:8000/api/register`, {
-				name,
-				email,
-				password,
+			const res = await axios.post(
+				`${process.env.REACT_APP_API}/register`,
+				{
+					name,
+					email,
+					password,
+				}
+			);
+			console.log('REGISTER USER ===> ', res);
+			toast.success('Register success. Please login!', {
+				position: 'bottom-right',
+				autoClose: 5000,
+				hideProgressBar: false,
+				closeOnClick: true,
+				pauseOnHover: true,
+				draggable: true,
+				progress: undefined,
 			});
-			console.log('REGISTER USER ===>', res);
-		} catch (error) {
-			console.log(error);
+			history.push('/login');
+		} catch (err) {
+			console.log(err);
+			if (err.response.status === 400) toast.error(err.response.data);
 		}
 
 		setName('');
