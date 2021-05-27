a9636331ae7a0830246f11f30d993f9275909f29
mahovsson <matus137@gmail.com>
2021-05-27 09:12:49 +0200

    register function moved to actions

--- src/auth/Register.js
+++ src/auth/Register.js
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
 import RegisterForm from '../components/RegisterForm';
-import axios from 'axios';
 import { toast } from 'react-toastify';
 import { useHistory } from 'react-router-dom';
+import { register } from '../actions/auth';
 
 const Register = () => {
 	const [name, setName] = useState('');
@@ -13,14 +13,11 @@ const Register = () => {
 	const handleSubmit = async (e) => {
 		e.preventDefault();
 		try {
-			const res = await axios.post(
-				`${process.env.REACT_APP_API}/register`,
-				{
-					name,
-					email,
-					password,
-				}
-			);
+			const res = await register({
+				name,
+				email,
+				password,
+			});
 			console.log('REGISTER USER ===> ', res);
 			toast.success('Register success. Please login!', {
 				position: 'bottom-right',
